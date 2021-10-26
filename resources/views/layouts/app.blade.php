0a53438f1add9cab5eaa87ddfa1a7b1b8a127eec
Junior Rodrigues <spider_junior01@hotmail.com>
2021-10-26 15:00:48 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,17 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">Inicio</a>
+
+                <div class="nav-item dropdown">
+                    <a class="navbar-brand dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Administração</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="">Usuários</a>
+                        <a class="dropdown-item" href="">Permissões</a>
+                      
+                    </div>
+                </div>    
+            
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
