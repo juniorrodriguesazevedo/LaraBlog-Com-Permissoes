f2b1b63d0803b83ff2c55233f1f27b3ae9aeb9ca
Junior Rodrigues <spider_junior01@hotmail.com>
2021-10-29 14:21:07 -0300

    add Observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserObserver
+{
+    /**
+     * Handle the User "creating" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function creating(User $user)
+    {
+        $user->name = Str::title($user->name);
+        $user->password = Hash::make($user->password);
+    }
+
+    /**
+     * Handle the User "updating" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updating(User $user)
+    {
+        $user->name = Str::title($user->name);
+        $user->password = Hash::make($user->password);
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
